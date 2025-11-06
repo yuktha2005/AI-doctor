0590533dafa60e385e6245981fab9f1e28d6cc6c
Yuktha <yukthaa2005@gmail.com>
2025-11-06 22:46:56 +0530

    Initial commit

--- brain_doctor.py
+++ brain_doctor.py
@@ -1,1 +1,14 @@
 import os
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+
+import base64
+image_path="acne.jpg"
+image_file=open(image_path,"rb")
+encoded_image= base64.b64encode(image_file.read()).decode("utf-8")
+
+from groq import Groq
+
+client=Groq()
+model="qwen/qwen3-32b"
+
+
